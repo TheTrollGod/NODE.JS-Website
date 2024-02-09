fdbd08f7d77a85016b8890c8d9d333f9bd641235
Anthony <anthony@trollgod.xyz>
2024-02-09 12:22:29 -0700

    Delete websites.js
    Needed for proper tracking

--- websites.js
+++
@@ -1,7 +0,0 @@
-//Declare your websites here
-const websites = {
-    'example.com': '/path/to/example.com/files',
-    'www.example2.com': '/path/to/example2.com/files'
-};
-
-module.exports = websites;
