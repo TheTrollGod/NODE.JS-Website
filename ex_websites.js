b0181459aade7b640844d208516d4c754a9543f3
Anthony Sherman <anthony@trollgod.xyz>
2024-02-09 12:21:20 -0700

    Created example file for Website Abstraction

---
+++ ex_websites.js
@@ -0,0 +1,8 @@
+//Change file name to "websites.js" for import to correctly work
+//Declare your websites here
+const websites = {
+    'example.com': '/path/to/example.com/files',
+    'www.example2.com': '/path/to/example2.com/files'
+};
+
+module.exports = websites;
